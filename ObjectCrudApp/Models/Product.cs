94981c8b98e0d2599f60f371e60a007b130e55dc
bibashmanjusubedi <acharabibash@outlook.com>
2025-04-04 07:47:11 +0545

    Price Model in Database for Entity Framework

--- ObjectCrudApp/Models/Product.cs
+++ ObjectCrudApp/Models/Product.cs
@@ -5,7 +5,11 @@ namespace ObjectCrudApp.Models
     public class Product
     {
         public int Id { get; set;}
+
+        [Display(Name="NepaliNaam")]
         public string Name { get; set;} = string.Empty;
 
+        [Display(Name="Price(Rs.)")]
+        public int Price {get;set;} = 0;
     }
 }
